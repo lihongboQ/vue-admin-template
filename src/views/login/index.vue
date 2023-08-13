430c3349639f2d2df4f1db3ecfa3895314d251bf
Ganseblum <977252487@qq.com>
2023-08-13 20:13:00 +0800

    update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,8 +57,8 @@
       >
       <div class="flex">
         <div class="tips">
-          <span style="margin-right: 20px">username: admin</span>
-          <span> password: any</span>
+          <!-- <span style="margin-right: 20px">username: admin</span>
+          <span> password: any</span> -->
         </div>
         <div>
           <!-- <el-button type="primary">注册</el-button> -->
